b9dd378580ec0da41d5094eebd5e22d13ba81e42
Kaja Radošević <73023084+KajaRado7@users.noreply.github.com>
2020-11-02 12:51:04 +0100

    Update JS-203.js

--- JS-203.js
+++ JS-203.js
@@ -1,8 +1,8 @@
 let velicina = 9;
 let red = '';
-for(let i=1; i<velicina; i++){ //Y-axis
+for(let i=1; i<velicina; i++){ 
 
-  for(let j=1; j<velicina; j++){ //X-axis
+  for(let j=1; j<velicina; j++){ 
     if(i % 2 == 0){
       if(j % 2 ==0){
         red += '#'
